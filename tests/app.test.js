877eec5671bd0cf006177953dc56b3627609cea0
erbroome <ellenehall@gmail.com>
2026-02-07 09:29:41 -0600

    add basic config files and cleanup repo

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,15 +1,13 @@
 // import testing tools
 import { describe, it, expect } from "vitest";
-// import supertest to simulate HTTP requests (API testing)
 import request from "supertest";
-// import the Express app from app.js
 import app from "../src/app.js";
 
-//start of tests by sending a fake GET request to "/"
-describe("GET /", () => {
-  it("returns welcome JSON", async () => {
-    const res = await request(app).get("/");
-    expect(res.status).toBe(200);
-    expect(res.body).toHaveProperty("message");
+describe("GET /items", () => {
+  it("returns an array of items with status 200", async () => {
+    const response = await request(app).get("/items");
+
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
   });
 });
